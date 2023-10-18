4afaf81d10c276a77ece1b8fbff83462f64ea178
Camilo <juguevah@unal.edu.co>
2023-10-18 00:52:40 -0500

    Config the logic of the music time

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,46 @@ use std::io::BufReader;
 use std::io::BufWriter;
 mod notas;
 
-fn export(text: String, config: (u32, (u32, u32))) {
+fn export(text: String, configuration: (u32, (u32, u32), String)) {
     let (doc, page1, layer1) = PdfDocument::new(
-        "PDF_Document_title",
-        Mm(config.1 .0 as f32),
-        Mm(config.1 .1 as f32),
+        "",
+        Mm(configuration.1 .0 as f32),
+        Mm(configuration.1 .1 as f32),
         "Layer 1",
     );
     let current_layer = doc.get_page(page1).get_layer(layer1);
-    let font = doc
-        .add_external_font(File::open("../../../Downloads/lassus/LASSUS.TTF").unwrap())
+    let default_font = doc
+        .add_external_font(
+            // Warn: Change this to relative path
+            File::open("../../../Downloads/DejaVuSansMono/DejaVuSansMNerdFont-Bold.ttf").unwrap(),
+        )
         .unwrap();
-    current_layer.use_text(text, config.0 as f32, Mm(10.0), Mm(100.0), &font);
-    doc.save(&mut BufWriter::new(
-        File::create("test_working.pdf").unwrap(),
-    ))
-    .unwrap();
+    let music_font = doc
+        .add_external_font(File::open("../../../Downloads/lassus/LassusV2.otf").unwrap())
+        .unwrap();
+
+    current_layer.use_text(
+        configuration.2,
+        12.0,
+        Mm(configuration.1 .0 as f32 / 3.0),
+        Mm(configuration.1 .1 as f32 - 10.0),
+        &default_font,
+    );
+    current_layer.use_text(
+        text,
+        configuration.0 as f32,
+        Mm(10.0),
+        Mm(100.0),
+        &music_font,
+    );
+
+    doc.save(&mut BufWriter::new(File::create("Partitura.pdf").unwrap()))
+        .unwrap();
 }
 
-fn input() -> std::io::Result<String> {
-    let archivo = File::open("Input.txt")?;
-    let buf_reader = BufReader::new(archivo);
-    let mut section = Vec::new();
-    for lines in buf_reader.lines() {
-        section.push(lines.unwrap())
-    }
-    Ok(section.concat())
-}
 
-fn search(parametro: &str, input: String) -> Vec<String> {
+// Perf: I don't like the config function and the regex_search function
+fn regex_search(parametro: &str, input: String) -> Vec<String> {
     let re = Regex::new(&parametro).unwrap();
     let captures = re.captures_iter(&input);
     let mut strings = Vec::new();
@@ -44,11 +55,11 @@ fn search(parametro: &str, input: String) -> Vec<String> {
     }
     return strings;
 }
-// FIXME
-// Considero que esto no está bien optimizado
-fn config() -> (u32, (u32, u32)) {
-    let psize = search(r"(?:psize)\(([^)]+)\)", input().unwrap());
-    let fsize = search(r"(?:fsize)\(([^)]+)\)", input().unwrap());
+
+fn config() -> (u32, (u32, u32), String) {
+    let psize = regex_search(r"(?:psize)\(([^)]+)\)", input_file().unwrap());
+    let fsize = regex_search(r"(?:fsize)\(([^)]+)\)", input_file().unwrap());
+    let title = regex_search(r"(?:title)\(([^)]+)\)", input_file().unwrap());
     let mut psize_vec = Vec::new();
     let mut fsize_vec = Vec::new();
     for i in psize {
@@ -61,43 +72,119 @@ fn config() -> (u32, (u32, u32)) {
         fsize_vec.push(i.parse().unwrap())
     }
     let psize_vec = psize_vec;
-    (fsize_vec[0], (psize_vec[0], psize_vec[1]))
+    (fsize_vec[0], (psize_vec[0], psize_vec[1]), title.concat())
 }
 
-
-fn replace_notes_font(search: Vec<String>) -> String {
+fn replace_to_lassusfont(search: Vec<Vec<String>>) -> String {
     let mut union = Vec::new();
     for i in search {
-        if let Some(replacement) = notas::sol().get(&i) {
-            union.push(replacement.to_owned())
-        } else {
-            union.push(" ".to_string())
+        for e in i {
+            if let Some(replacement) = notas::sol().get(&e) {
+                union.push(replacement.to_owned())
+            } else {
+                union.push(" ".to_string())
+            }
         }
     }
+    println!("{}", union.concat());
     return union.concat();
 }
 
-fn compas() -> std::io::Result<String> {
-    let notas = search(r"(?:nota)\(([^)]+)\)", input().unwrap());
-    let compas: f32 = search(r"(?:compas)\(([^)]+)\)", input().unwrap())
-        .concat()
-        .parse()
-        .unwrap();
-    println!("{}", compas);
-    println!("{:?}", notas);
-    let mut result = 0.0;
-    for i in notas {
-        let number: f32 = i.chars().next().unwrap().to_string().parse().unwrap();
-        if result >= compas {
-            return Ok("-".to_string());
-        } else {
-            result += 1.0 / number;
-            continue;
+fn relative_bar(input: &str) -> f32 {
+    match input {
+        "2/4" => 0.5,
+        "3/4" => 0.75,
+        "4/4" => 1.0,
+        "3/2" => 1.5,
+        _ => 0.0,
+    }
+}
+
+fn input_file() -> std::io::Result<String> {
+    let archivo = File::open("Input.txt")?;
+    let buf_reader = BufReader::new(archivo);
+    let mut section = Vec::new();
+    for lines in buf_reader.lines() {
+        section.push(lines.unwrap())
+    }
+    Ok(section.concat())
+}
+
+fn search_times(texto: &str) -> Vec<Vec<String>> {
+    let re = Regex::new(r"\((\d+/\d+)\)\{([^}]*)\}").unwrap();
+    let mut container_notes = Vec::new();
+    let mut subvector = Vec::new();
+
+    for cap in re.captures_iter(texto) {
+        let tiempo = &cap[1];
+        let contenido = &cap[2];
+        let re_notas = Regex::new(r"(?:nota|clave)\(([^)]+)\)").unwrap();
+
+        if !subvector.is_empty() {
+            container_notes.push(subvector.clone());
+            subvector.clear();
+        }
+
+        subvector.push(tiempo.to_string());
+
+        for nota in re_notas.captures_iter(contenido) {
+            let contenido_nota = &nota[1];
+            subvector.push(contenido_nota.to_string());
         }
     }
-    Ok("".to_string())
+
+    if !subvector.is_empty() {
+        container_notes.push(subvector);
+    }
+
+    println!("{:?}", container_notes);
+    container_notes
+}
+
+fn times_logic(input: Vec<Vec<String>>) -> Vec<Vec<String>> {
+    let mut container_vectors = Vec::new();
+
+    for item in input {
+        let mut vectors = Vec::new();
+        let mut result = 0.0;
+
+        for (index, elements) in item.iter().enumerate() {
+            if index == 0 {
+                if index + 1 < item.len() {
+                    vectors.push(item[index + 1].to_string());
+                }
+            } else if index == 1 {
+                vectors.push(item[0].to_string());
+            } else {
+                let numero: f32 = elements
+                    .chars()
+                    .next()
+                    .unwrap()
+                    .to_string()
+                    .parse()
+                    .unwrap_or(0.0);
+                let tempo = relative_bar(&item[0]);
+
+                if (1.0 / numero) + result == tempo {
+                    vectors.push(elements.to_string());
+                    vectors.push("-".to_string());
+                    result = 0.0;
+                } else {
+                    result += 1.0 / numero;
+                    vectors.push(elements.to_string());
+                }
+            }
+        }
+
+        container_vectors.push(vectors);
+    }
+
+    println!("{:?}", container_vectors);
+    container_vectors
 }
 
 fn main() {
-    println!("{}", compas().unwrap())
+    let input = search_times(&input_file().unwrap());
+    let hola = replace_to_lassusfont(times_logic(input));
+    export(hola, config())
 }
--- src/notas.rs
+++ src/notas.rs
@@ -3,122 +3,234 @@ use std::collections::HashMap;
 pub fn sol() -> HashMap<String, String> {
     let mut input_to_char = HashMap::new();
 
+    // Claves
     input_to_char.insert("G".to_string(), "`".to_string());
+    input_to_char.insert("F".to_string(), "1".to_string());
+    input_to_char.insert("C1".to_string(), "2".to_string());
+    input_to_char.insert("C2".to_string(), "3".to_string());
 
-    input_to_char.insert("cmp".to_string(), "-".to_string());
+    //Silents
+    input_to_char.insert("8s".to_string(), "7".to_string());
+    input_to_char.insert("4s".to_string(), "8".to_string());
+    input_to_char.insert("2s".to_string(), "9".to_string());
+    input_to_char.insert("1s".to_string(), "0".to_string());
 
+    // Bar and spacing
+    input_to_char.insert("-".to_string(), "-".to_string());
+    input_to_char.insert("6".to_string(), "6".to_string());
 
+    //compas
+    input_to_char.insert("4/4".to_string(), "ñ".to_string());
+    input_to_char.insert("3/4".to_string(), "×".to_string());
+    input_to_char.insert("2/4".to_string(), "ó".to_string());
 
-    // Mapeos para las notas naturales (sin bemoles ni sostenidos)
+    // LA
     input_to_char.insert("4A".to_string(), ")".to_string());
     input_to_char.insert("2A".to_string(), "c".to_string());
     input_to_char.insert("1A".to_string(), "o".to_string());
 
-    // Mapeos para las notas con bemol
-    input_to_char.insert("4Ab".to_string(), ")?".to_string());
-    input_to_char.insert("2Ab".to_string(), "c?".to_string());
-    input_to_char.insert("1Ab".to_string(), "o?".to_string());
+    input_to_char.insert("4Ab".to_string(), "?)".to_string());
+    input_to_char.insert("2Ab".to_string(), "?c".to_string());
+    input_to_char.insert("1Ab".to_string(), "?o".to_string());
 
-    // Mapeos para las notas con sostenido
-    input_to_char.insert("4A#".to_string(), "4lasostenido".to_string());
-    input_to_char.insert("2A#".to_string(), "2lasostenido".to_string());
-    input_to_char.insert("1A#".to_string(), "1lasostenido".to_string());
+    input_to_char.insert("4A#".to_string(), "Á)".to_string());
+    input_to_char.insert("2A#".to_string(), "Ác".to_string());
+    input_to_char.insert("1A#".to_string(), "Áo".to_string());
 
+    // Si
     input_to_char.insert("4B".to_string(), "_".to_string());
     input_to_char.insert("2B".to_string(), "v".to_string());
     input_to_char.insert("1B".to_string(), "]".to_string());
 
-    // Mapeos para las notas con bemol con "B" en lugar de "A"
-    /* nota_to_char.insert(
-        "4Bb".to_string(),
-        char::from_u32(0164).unwrap().to_string() + "_",
-    );
-    nota_to_char.insert(
-        "2Bb".to_string(),
-        char::from_u32(0164).unwrap().to_string() + "v",
-    );
-    nota_to_char.insert(
-        "1Bb".to_string(),
-        char::from_u32(0164).unwrap().to_string() + "]",
-    ); */
+    input_to_char.insert("4Bb".to_string(), "Ÿ-".to_string());
+    input_to_char.insert("2Bb".to_string(), "Ÿv".to_string());
+    input_to_char.insert("1Bb".to_string(), "Ÿ]".to_string());
 
-    // Mapeos para las notas con sostenido con "B" en lugar de "A"
-    input_to_char.insert("4B#".to_string(), "4bbemol".to_string());
-    input_to_char.insert("2B#".to_string(), "2bsostenido".to_string());
-    input_to_char.insert("1B#".to_string(), "1bsostenido".to_string());
+    input_to_char.insert("4B#".to_string(), "Ë-".to_string());
+    input_to_char.insert("2B#".to_string(), "Ëv".to_string());
+    input_to_char.insert("1B#".to_string(), "Ë]".to_string());
 
+    // Do
     input_to_char.insert("4C".to_string(), "+".to_string());
     input_to_char.insert("2C".to_string(), "l".to_string());
     input_to_char.insert("1C".to_string(), "r".to_string());
 
-    // Mapeos para las notas con bemol con "C" en lugar de "B"
-    input_to_char.insert("4Cb".to_string(), "B+".to_string());
-    input_to_char.insert("2Cb".to_string(), "Bl".to_string());
-    input_to_char.insert("1Cb".to_string(), "Br".to_string());
+    input_to_char.insert("4Cb".to_string(), "·+".to_string());
+    input_to_char.insert("2Cb".to_string(), "·l".to_string());
+    input_to_char.insert("1Cb".to_string(), "·r".to_string());
 
-    // Mapeos para las notas con sostenido con "C" en lugar de "B"
-    input_to_char.insert("4C#".to_string(), "4csostenido".to_string());
-    input_to_char.insert("2C#".to_string(), "2csostenido".to_string());
-    input_to_char.insert("1C#".to_string(), "1csostenido".to_string());
+    input_to_char.insert("4C#".to_string(), "È+".to_string());
+    input_to_char.insert("2C#".to_string(), "Èl".to_string());
+    input_to_char.insert("1C#".to_string(), "Èr".to_string());
 
-    // Mapeos para las notas naturales (sin bemoles ni sostenidos) con "D" en lugar de "C"
+    // Re
+
     input_to_char.insert("4D".to_string(), "^".to_string());
     input_to_char.insert("2D".to_string(), ";".to_string());
     input_to_char.insert("1D".to_string(), "t".to_string());
 
-    // Mapeos para las notas con bemol con "D" en lugar de "C"
     input_to_char.insert("4Db".to_string(), "N^".to_string());
     input_to_char.insert("2Db".to_string(), "N;".to_string());
     input_to_char.insert("1Db".to_string(), "Nt".to_string());
 
-    // Mapeos para las notas con sostenido con "D" en lugar de "C"
-    input_to_char.insert("4D#".to_string(), "4dsostenido".to_string());
-    input_to_char.insert("2D#".to_string(), "2dsostenido".to_string());
-    input_to_char.insert("1D#".to_string(), "1dsostenido".to_string());
+    input_to_char.insert("4D#".to_string(), "„^".to_string());
+    input_to_char.insert("2D#".to_string(), "„;".to_string());
+    input_to_char.insert("1D#".to_string(), "„t".to_string());
 
-    // Mapeos para las notas naturales (sin bemoles ni sostenidos) con "E" en lugar de "D"
+    // Mi
     input_to_char.insert("4E".to_string(), "&".to_string());
     input_to_char.insert("2E".to_string(), "'".to_string());
     input_to_char.insert("1E".to_string(), "y".to_string());
 
-    // Mapeos para las notas con bemol con "E" en lugar de "D"
     input_to_char.insert("4Eb".to_string(), "M&".to_string());
     input_to_char.insert("2Eb".to_string(), "M'".to_string());
     input_to_char.insert("1Eb".to_string(), "My".to_string());
 
-    // Mapeos para las notas con sostenido con "E" en lugar de "D"
-    input_to_char.insert("4E#".to_string(), "4esostenido".to_string());
-    input_to_char.insert("2E#".to_string(), "2esostenido".to_string());
-    input_to_char.insert("1E#".to_string(), "1esostenido".to_string());
+    input_to_char.insert("4E#".to_string(), "‰&".to_string());
+    input_to_char.insert("2E#".to_string(), "‰'".to_string());
+    input_to_char.insert("1E#".to_string(), "‰y".to_string());
 
-    // Mapeos para las notas naturales (sin bemoles ni sostenidos) con "F" en lugar de "E"
+    // Fa
     input_to_char.insert("4F".to_string(), "*".to_string());
     input_to_char.insert("2F".to_string(), "z".to_string());
     input_to_char.insert("1F".to_string(), "u".to_string());
 
-    // Mapeos para las notas con bemol con "F" en lugar de "E"
     input_to_char.insert("4Fb".to_string(), "<*".to_string());
     input_to_char.insert("2Fb".to_string(), "<z".to_string());
     input_to_char.insert("1Fb".to_string(), "<u".to_string());
 
-    // Mapeos para las notas con sostenido con "F" en lugar de "E"
-    input_to_char.insert("4F#".to_string(), "4fsostenido".to_string());
-    input_to_char.insert("2F#".to_string(), "2fsostenido".to_string());
-    input_to_char.insert("1F#".to_string(), "1fsostenido".to_string());
+    input_to_char.insert("4F#".to_string(), "Â*".to_string());
+    input_to_char.insert("2F#".to_string(), "Âz".to_string());
+    input_to_char.insert("1F#".to_string(), "Âu".to_string());
 
-    // Mapeos para las notas naturales (sin bemoles ni sostenidos) con "G" en lugar de "F"
+    // Sol
     input_to_char.insert("4G".to_string(), "(".to_string());
     input_to_char.insert("2G".to_string(), "x".to_string());
     input_to_char.insert("1G".to_string(), "i".to_string());
 
-    // Mapeos para las notas con bemol con "G" en lugar de "F"
     input_to_char.insert("4Gb".to_string(), ">(".to_string());
     input_to_char.insert("2Gb".to_string(), ">x".to_string());
     input_to_char.insert("1Gb".to_string(), ">i".to_string());
 
-    // Mapeos para las notas con sostenido con "G" en lugar de "F"
-    input_to_char.insert("4G#".to_string(), "4gsostenido".to_string());
-    input_to_char.insert("2G#".to_string(), "2gsostenido".to_string());
-    input_to_char.insert("1G#".to_string(), "1gsostenido".to_string());
+    input_to_char.insert("4G#".to_string(), "Ê(".to_string());
+    input_to_char.insert("2G#".to_string(), "Êx".to_string());
+    input_to_char.insert("1G#".to_string(), "Êi".to_string());
+    input_to_char
+}
+pub fn fa() -> HashMap<String, String> {
+    let mut input_to_char = HashMap::new();
+
+    // Claves
+    input_to_char.insert("G".to_string(), "`".to_string());
+    input_to_char.insert("F".to_string(), "1".to_string());
+    input_to_char.insert("C1".to_string(), "2".to_string());
+    input_to_char.insert("C2".to_string(), "3".to_string());
+
+    //Silents
+    input_to_char.insert("8s".to_string(), "7".to_string());
+    input_to_char.insert("4s".to_string(), "8".to_string());
+    input_to_char.insert("2s".to_string(), "9".to_string());
+    input_to_char.insert("1s".to_string(), "0".to_string());
+
+    // Bar and spacing
+    input_to_char.insert("-".to_string(), "-".to_string());
+    input_to_char.insert("6".to_string(), "6".to_string());
+
+    //compas
+    input_to_char.insert("4/4".to_string(), "ñ".to_string());
+    input_to_char.insert("3/4".to_string(), "×".to_string());
+    input_to_char.insert("2/4".to_string(), "ó".to_string());
+
+    // LA
+    input_to_char.insert("4A".to_string(), ")".to_string());
+    input_to_char.insert("2A".to_string(), "c".to_string());
+    input_to_char.insert("1A".to_string(), "o".to_string());
+
+    input_to_char.insert("4Ab".to_string(), "?)".to_string());
+    input_to_char.insert("2Ab".to_string(), "?c".to_string());
+    input_to_char.insert("1Ab".to_string(), "?o".to_string());
+
+    input_to_char.insert("4A#".to_string(), "Á)".to_string());
+    input_to_char.insert("2A#".to_string(), "Ác".to_string());
+    input_to_char.insert("1A#".to_string(), "Áo".to_string());
+
+    // Si
+    input_to_char.insert("4B".to_string(), "_".to_string());
+    input_to_char.insert("2B".to_string(), "v".to_string());
+    input_to_char.insert("1B".to_string(), "]".to_string());
+
+    input_to_char.insert("4Bb".to_string(), "Ÿ-".to_string());
+    input_to_char.insert("2Bb".to_string(), "Ÿv".to_string());
+    input_to_char.insert("1Bb".to_string(), "Ÿ]".to_string());
+
+    input_to_char.insert("4B#".to_string(), "Ë-".to_string());
+    input_to_char.insert("2B#".to_string(), "Ëv".to_string());
+    input_to_char.insert("1B#".to_string(), "Ë]".to_string());
+
+    // Do
+    input_to_char.insert("4C".to_string(), "+".to_string());
+    input_to_char.insert("2C".to_string(), "l".to_string());
+    input_to_char.insert("1C".to_string(), "r".to_string());
+
+    input_to_char.insert("4Cb".to_string(), "·+".to_string());
+    input_to_char.insert("2Cb".to_string(), "·l".to_string());
+    input_to_char.insert("1Cb".to_string(), "·r".to_string());
+
+    input_to_char.insert("4C#".to_string(), "È+".to_string());
+    input_to_char.insert("2C#".to_string(), "Èl".to_string());
+    input_to_char.insert("1C#".to_string(), "Èr".to_string());
+
+    // Re
+
+    input_to_char.insert("4D".to_string(), "Q".to_string());
+    input_to_char.insert("2D".to_string(), "n".to_string());
+    input_to_char.insert("1D".to_string(), "]".to_string());
+
+    input_to_char.insert("4Db".to_string(), "N^".to_string());
+    input_to_char.insert("2Db".to_string(), "N;".to_string());
+    input_to_char.insert("1Db".to_string(), "Nt".to_string());
+
+    input_to_char.insert("4D#".to_string(), "„^".to_string());
+    input_to_char.insert("2D#".to_string(), "„;".to_string());
+    input_to_char.insert("1D#".to_string(), "„t".to_string());
+
+    // Mi
+    input_to_char.insert("4E".to_string(), "&".to_string());
+    input_to_char.insert("2E".to_string(), "'".to_string());
+    input_to_char.insert("1E".to_string(), "y".to_string());
+
+    input_to_char.insert("4Eb".to_string(), "M&".to_string());
+    input_to_char.insert("2Eb".to_string(), "M'".to_string());
+    input_to_char.insert("1Eb".to_string(), "My".to_string());
+
+    input_to_char.insert("4E#".to_string(), "‰&".to_string());
+    input_to_char.insert("2E#".to_string(), "‰'".to_string());
+    input_to_char.insert("1E#".to_string(), "‰y".to_string());
+
+    // Fa
+    input_to_char.insert("4F".to_string(), "*".to_string());
+    input_to_char.insert("2F".to_string(), "z".to_string());
+    input_to_char.insert("1F".to_string(), "u".to_string());
+
+    input_to_char.insert("4Fb".to_string(), "<*".to_string());
+    input_to_char.insert("2Fb".to_string(), "<z".to_string());
+    input_to_char.insert("1Fb".to_string(), "<u".to_string());
+
+    input_to_char.insert("4F#".to_string(), "Â*".to_string());
+    input_to_char.insert("2F#".to_string(), "Âz".to_string());
+    input_to_char.insert("1F#".to_string(), "Âu".to_string());
+
+    // Sol
+    input_to_char.insert("4G".to_string(), "(".to_string());
+    input_to_char.insert("2G".to_string(), "x".to_string());
+    input_to_char.insert("1G".to_string(), "i".to_string());
+
+    input_to_char.insert("4Gb".to_string(), ">(".to_string());
+    input_to_char.insert("2Gb".to_string(), ">x".to_string());
+    input_to_char.insert("1Gb".to_string(), ">i".to_string());
+
+    input_to_char.insert("4G#".to_string(), "Ê(".to_string());
+    input_to_char.insert("2G#".to_string(), "Êx".to_string());
+    input_to_char.insert("1G#".to_string(), "Êi".to_string());
     input_to_char
 }
